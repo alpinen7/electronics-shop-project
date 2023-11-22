076f4fa5fd4100f72fb16b3eecc95379a4a9bdc9
Alexey Lebedev <lazdanny@mail.ru>
2023-11-22 17:51:44 +0300

    реализована логика программы ДЗ_1 ООП, разработаны тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item_1 = Item("Чехол", 200, 10)
+
+def test_calculate_total_price():
+    assert item_1.calculate_total_price() == 2000
+
+def test_apply_discount():
+    assert item_1.apply_discount() == None
+
+
+
+
